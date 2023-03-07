76b41e9283e5cdbf4a8626486615a10422c996bb
Adam <asheeres90@gmail.com>
2023-03-07 16:44:27 -0500

    Added Material 3 Theme

--- plant_tracker/lib/My_Plants_Page.dart
+++ plant_tracker/lib/My_Plants_Page.dart
@@ -8,7 +8,7 @@ Widget getHomePage(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
         title: const Text("My Plants"),
-        backgroundColor: const Color.fromARGB(255, 156, 232, 94),
+        backgroundColor: Color.fromARGB(255, 44, 71, 22),
         centerTitle: true,
         titleTextStyle:
             const TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
@@ -38,92 +38,98 @@ Widget genPlantTile(int number, context) {
       onTap: () {
         log("Moving to page ${curPlant.plant_name}"); //HERE IS WHERE YOU ADD THE 'PLANT PAGE'
       },
-      child: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                image: NetworkImage(curPlant.imageUrl),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.6),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            curPlant.room?.name ?? "",
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        )),
-                    Container(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(
-                            Icons.favorite,
-                            color: curPlant.isFavourite
-                                ? Colors.red
-                                : Colors.white,
-                          ),
-                        )),
-                  ],
-                ),
-                Container(
-                  alignment: Alignment.bottomLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                curPlant.plant_name,
-                                style: const TextStyle(
-                                    fontSize: 24.0, color: Colors.white),
-                              ))),
-                      SizedBox(
-                        height: 40.0, // add a height to the container
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Icon(Icons.access_time, color: Colors.white),
-                            Text((curPlant.water_days).toString(),
-                                style: const TextStyle(color: Colors.white)),
-                            Expanded(
-                              child: LinearProgressIndicator(
-                                value: getWateringBar(
-                                    curPlant), // This should be a value between 0.0 and 1.0
-                                backgroundColor: Colors.grey,
-                                valueColor: const AlwaysStoppedAnimation<Color>(
-                                    Color.fromARGB(255, 76, 222, 241)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+      child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(curPlant.imageUrl),
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
-            ),
-          ),
-        ],
-      ));
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.6),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                curPlant.room?.name ?? "",
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            )),
+                        Container(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(
+                                Icons.favorite,
+                                color: curPlant.isFavourite
+                                    ? Colors.red
+                                    : Colors.white,
+                              ),
+                            )),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    curPlant.plant_name,
+                                    style: const TextStyle(
+                                        fontSize: 24.0, color: Colors.white),
+                                  ))),
+                          SizedBox(
+                            height: 40.0, // add a height to the container
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Icon(Icons.access_time,
+                                    color: Colors.white),
+                                Text((curPlant.water_days).toString(),
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                                Expanded(
+                                  child: LinearProgressIndicator(
+                                    value: getWateringBar(
+                                        curPlant), // This should be a value between 0.0 and 1.0
+                                    backgroundColor: Colors.grey,
+                                    valueColor:
+                                        const AlwaysStoppedAnimation<Color>(
+                                            Color.fromARGB(255, 76, 222, 241)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )));
 }
 
 // double getWateringBar(Plant curPlant, context) {
--- plant_tracker/lib/tempCodeRunnerFile.dart
+++ plant_tracker/lib/tempCodeRunnerFile.dart
@@ -1,1 +1,1 @@
-
+genPlantTile
